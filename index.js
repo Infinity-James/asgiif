494bf2a452bc7ecebe82570ca3689ab65a2a78be
James Valaitis <james@infinityjames.com>
2016-02-24 19:30:15 +0000

    Refactored index.js

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const http = require("http");
 
-http.createServer(function(request, response) {
+//  use the port 3000 because that's what people do
+const port = 3000;
+
+/**
+ *  This is the function that will be called when the server is hit.
+ **/
+function handleRequest(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello World\n');
-}).listen(3000, 'localhost');
+}
+
+var server = http.createServer(handleRequest));
+
+server.listen(port, 'localhost');
